179acaaf4acad6ff7726ec72f16147add54cdc77
Sam Lam <90791465+samlam67812@users.noreply.github.com>
2026-02-01 22:57:37 +0800

    reset the project

--- trading-engine/src/main/java/com/itranswarp/exchange/match/MatchDetailRecord.java
+++
@@ -1,8 +0,0 @@
-package com.itranswarp.exchange.match;
-
-import com.itranswarp.exchange.model.trade.OrderEntity;
-
-import java.math.BigDecimal;
-
-public record MatchDetailRecord(BigDecimal price, BigDecimal quantity, OrderEntity takerOrder, OrderEntity makerOrder) {
-}
--- trading-engine/src/main/java/com/itranswarp/exchange/match/OrderKey.java
+++
@@ -1,6 +0,0 @@
-package com.itranswarp.exchange.match;
-
-import java.math.BigDecimal;
-
-public record OrderKey(long sequenceId, BigDecimal price) {
-}
